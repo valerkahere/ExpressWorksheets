534aed04ab1145a197f8bdd4fdd2a5d426b5e16c
valerkahere <valeranavalni@gmail.com>
2026-09-16 10:49:23 +0100

    feat: add more routes and middleware

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,26 @@ const PORT = process.env.PORT || 2333;
 
 const app: Application = express();
 
+app.use((req, _res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.get('/ping', async (_req: Request, res: Response) => {
   res.json({
     message: 'hello from valerkahere',
+  });
+});
+
+app.get('/bananas', async (_req: Request, res: Response) => {
+  res.json({
+    message: 'this is bananas',
+  });
+});
+
+app.get('/auth', async (_req: Request, res: Response) => {
+  res.json({
+    message: 'nothing here',
   });
 });
 
